8dbab3700ec4b990826f9b17cdf8290fe1296ab1
jake <jake@jakecoffman.com>
2019-10-14 19:10:43 -0500

    simplify

--- breakout/object.go
+++ breakout/object.go
@@ -2,9 +2,8 @@ package breakout
 
 import (
 	"fmt"
-	"math"
-	"github.com/jakecoffman/learnopengl/breakout/eng"
 	"github.com/go-gl/mathgl/mgl32"
+	"github.com/jakecoffman/learnopengl/breakout/eng"
 )
 
 type Object struct {
@@ -32,31 +31,7 @@ func NewGameObject(pos, size mgl32.Vec2, sprite *eng.Texture2D) *Object {
 func (o *Object) Draw(renderer *eng.SpriteRenderer, last *mgl32.Vec2, alpha float32) {
 	pos := o.Position
 	if last != nil {
-		pos = SLerp(*last, o.Position, alpha)
+		pos = pos.Mul(alpha).Add(last.Mul(1.0-alpha))
 	}
 	renderer.DrawSprite(o.Sprite, pos, o.Size, o.Rotation, o.Color)
-}
-
-func SLerp(v, other mgl32.Vec2, t float32) mgl32.Vec2 {
-	dot := v.Normalize().Dot(other.Normalize())
-	omega := float32(math.Acos(Clamp(float64(dot), -1, 1)))
-
-	if omega < 1e-3 {
-		return Lerp(v, other, t)
-	}
-
-	denom := 1.0 / math.Sin(float64(omega))
-	return v.Mul(float32(math.Sin(float64((1.0-t)*omega)) * denom)).Add(other.Mul(float32(math.Sin(float64(t*omega)) * denom)))
-}
-
-func Lerp(v, other mgl32.Vec2, t float32) mgl32.Vec2 {
-	return v.Mul(1.0 - t).Add(other.Mul(t))
-}
-
-func Clamp(f, min, max float64) float64 {
-	if f > min {
-		return math.Min(f, max)
-	} else {
-		return math.Min(min, max)
-	}
 }
